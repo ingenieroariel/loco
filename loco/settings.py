310fd740d154493e77e72d5011d8560f02dcf938
Ariel Nuñez <ingenieroariel@gmail.com>
2015-10-05 17:49:05 -0500

    Added osm app

--- loco/settings.py
+++ loco/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-)
+    'django.contrib.gis',
+    'osm',
+    )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
